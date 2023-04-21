2e566c827c6b3f646c43526be45ab98bea6a42ef
Millie Swain <swa19001@byui.edu>
2023-04-21 16:18:07 -0700

    move or rebuild in L01_personal

---
+++ l01_personal/index.js
@@ -0,0 +1,15 @@
+ var express = require('express');
+ var app = express();
+
+ app.set('port', process.env.PORT || 3000);
+ app.get("/", getData);
+
+function getData(req, res) {
+    console.log("getting data");
+    res.write("{\"name\":\"Millie\"}");
+    res.end();
+}
+
+app.listen(app.get("port"), () => console.log("server is listening on port " + app.get("port")));
+
+console.log("Millie Swain")
