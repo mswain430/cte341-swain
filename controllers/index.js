5f6c9dd46d84cf0ac2801995fe834fcfcaaca29b
Millie Swain <swa19001@byui.edu>
2023-04-21 16:37:40 -0700

    rebuild in new foldeer

--- controllers/index.js
+++
@@ -1,9 +0,0 @@
-displayJoke = (req, res) => {
-    const data = 
-       "joke here";
-    res.status(200).send(data);
-};
-
-Module.exports = {
-    displayJoke
-};
--- routes/index.js
+++
@@ -1,9 +0,0 @@
-const routes = require('express').Router();
-//const jokes = require('../controllers');
-
-routes.get('/', (req, res) => {
-    res.send('Rhoda Ready');
-});
-//routes.get('/', jokes.displayJoke);
-
-module.exports = routes;
