a1801360132504b1c42e4a8fb8bd294722808b48
Millie Swain <swa19001@byui.edu>
2023-04-22 20:41:40 -0700

    move items back to root

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ var express = require("express");
 var app = express();
 
 app.set("port", process.env.PORT || 3000);
-
 app.get("/", getData);
 
 function getData(req, res) {
---
+++ routes/index.js
@@ -0,0 +1,9 @@
+const routes = require('express').Router();
+//const jokes = require('../controllers');
+
+routes.get('/', (req, res) => {
+    res.send('Rhoda Ready');
+});
+//routes.get('/', jokes.displayJoke);
+
+module.exports = routes;
