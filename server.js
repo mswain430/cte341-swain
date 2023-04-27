69f966c7d25278297fe590cef6a69f5986c2101c
Millie Swain <swa19001@byui.edu>
2023-04-26 20:24:35 -0700

    server.js change

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const express = require('express');
+/* const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
 
@@ -6,4 +6,30 @@ app.use('/', require('./routes'))
 
 app.listen(port, () => {
     console.log(`Running on port ${port}`)
-})
+}) */
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+const mongodb = require('./db/connect');
+const contactsRoutes = require('./routes');
+
+const port = process.env.PORT || 8080;
+const app = express();
+
+app
+  .use(bodyParser.json())
+  .use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+  })
+  .use('/', contactsRoutes);
+
+mongodb.initDb((err, mongodb) => {
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(port);
+    console.log(`Connected to DB and listening on ${port}`);
+  }
+});
