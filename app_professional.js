2ea5efbfaf12f704a1c2481bbad0ad88b8990828
Millie Swain <swa19001@byui.edu>
2023-05-02 16:28:26 -0700

    package_json alignments

--- app_professional.js
+++ app_professional.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
 const mongodb = require('./db/connect');
+const professionalRoutes = require('./routes/professional');
 
 const port = process.env.PORT || 8080;
 const app = express();
@@ -11,9 +13,9 @@ app
     res.setHeader('Access-Control-Allow-Origin', '*');
     next();
   })
-  .use('/', require('./routes'));
+  .use('/professional', professionalRoutes);
 
-mongodb.initDb((err) => {
+mongodb.initDb((err, mongodb) => {
   if (err) {
     console.log(err);
   } else {
