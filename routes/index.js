f34ba1c0871fe661116092bb397f9b09cddc2283
Millie Swain <swa19001@byui.edu>
2023-05-05 16:57:33 -0700

    comment out unused code

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,4 @@
-const express = require('express');
+// const express = require('express');
 const router = Router();
 const contact = require('contacts');
 router.use('/contacts', require('./contacts'))
