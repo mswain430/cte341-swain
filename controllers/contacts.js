d0c87a7840dafec960107c4a562aebb370cbfd7b
Millie Swain <swa19001@byui.edu>
2023-05-06 20:27:45 -0700

    error handling

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,28 +1,35 @@
-//const express = require('express');
-//const bodyParser = require('body-parser');
 const mongodb = require('../db/connect');
 const ObjectId = require('mongodb').ObjectId;
 
 
 const getAll = async (req, res) => {
+  try {
   const result = await mongodb.getDb().db().collection('contacts').find();
   result.toArray().then((lists) => {
     res.setHeader('Content-Type', 'application/json');
     res.status(200).json(lists);
-  });
+  })
+} catch (err) {
+  res.status(500).json(err);
+}
 };
 
 const getSingle = async (req, res) => {
+  try {
   const userId = new ObjectId(req.params.id);
   const result = await mongodb.getDb().db().collection('contacts').find({ _id: userId });
   result.toArray().then((lists) => {
     res.setHeader('Content-Type', 'application/json');
     //res.setHeader('Content-Type', 'text/plain');
     res.status(200).json(lists);
-  });
+  })
+} catch (err) {
+  res.status(500).json(err);
+}
 };
 
 const createContact = async (req, res) => {
+  try {
   const contact = {
     firstName: req.body.firstName,
     lastName: req.body.lastName,
@@ -30,6 +37,7 @@ const createContact = async (req, res) => {
     favoriteColor: req.body.favoriteColor,
     birdthday: req.body.birthday
   };
+  console.log(req.body);
   const response = await mongodb.getDb().db().collection('contacts').insertOne(contact);
   if (response.acknowledged) {
     res.status(201).json(response);
@@ -37,6 +45,9 @@ const createContact = async (req, res) => {
   } else {
     res.status(500).json(response.error || 'Some error occurred while creating contact');
   }
+} catch (err) {
+  res.status(500).json(err);
+}
 };
 
 const updateContact = async (req, res) => {
