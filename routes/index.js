7dd8c18f3bf36b810be8c36a05ad68a7ad802323
Millie Swain <swa19001@byui.edu>
2023-05-12 15:40:48 -0700

    problem solving

--- routes/index.js
+++ routes/index.js
@@ -4,17 +4,17 @@ const routes = express.Router();
 routes.use('/', require('./swagger'))
 //routes.use('/contacts', require('./contacts'));
 //routes.use('temples', require('./temples'));  ***** problem child
-routes.use('flowers', require('./flowers'));
-routes.use('contacts', require('./gardenerContacts'))
+routes.use('/flowers', require('./flowers'));
+routes.use('/contacts', require('./gardenerContacts'))
 
- routes.use(
-  '/',
-  (docData = (req, res) => {
-    let docData = {
-      documentationURL: 'https://swain341-test.onrender.com/api-docs',
-    };
-    res.send(docData);
-  })
-); 
+ //routes.use(
+ // '/',
+ // (docData = (req, res) => {
+ //   let docData = {
+ //     documentationURL: 'https://swain341-test.onrender.com/api-docs',
+ //   };
+ //   res.send(docData);
+ // })
+//); 
 
 module.exports = routes;
